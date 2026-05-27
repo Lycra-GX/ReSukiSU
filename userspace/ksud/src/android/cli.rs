8e6bf8633d3aa6bec0b91b7b911468f86f69befa
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-05-27 22:41:07 +0800

    kernel,ksud: support module-provided init.rc injection (https://github.com/tiann/KernelSU/pull/3478)
    - The rc files in modules and the common directory will be merged and
    stored in `/metadata[/watchdog]/ksu/modules.rc`. These files will be
    appended to the main system rc file at system startup.
    - Module rc files: The `initrc/*.rc` files for each module.
    - Common rc files: All executable (chmod +x) `/data/adb/initrc.d/*.rc`
    files.
    - The `modules.rc` file will be regenerated after module changes
    (installation, disabling, uninstallation, etc.).
    - If a user modifies the common rc file, the `ksud initrc refresh`
    command can be run to regenerate the file and apply the changes.
    - Custom rc files can be ignored when patching bootimg using the
    `--no-custom-rc` option.
    
    ---------
    
    [cherry-picked from upstream commit https://github.com/tiann/KernelSU/commit/9ab31068345d5811160d74ff0dec79c51b1ce9c3]
    
    Co-authored-by: 5ec1cff <ewtqyqyewtqyqy@gmail.com>
    Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>

--- userspace/ksud/src/android/cli.rs
+++ userspace/ksud/src/android/cli.rs
@@ -8,10 +8,8 @@ use log::{LevelFilter, error, info};
 use crate::{
     android::{
         debug, dynamic_manager, feature, init_event, ksucalls,
-        module::{self, module_config},
-        profile, sepolicy, su, sulog,
-        susfs::{self},
-        umount_config, utils,
+        module::{self, module_config, regenerate_preinit_rc},
+        profile, sepolicy, su, sulog, umount_config, utils,
     },
     apk_sign, assets,
     boot_patch::{BootPatchArgs, BootRestoreArgs},
@@ -154,8 +152,11 @@ enum Commands {
     /// Resetprop - Magisk-compatible system property tool
     Resetprop(crate::android::resetprop::Args),
 
-    /// Manage susfs component
-    Susfs(susfs::cli::SusfsArgs),
+    /// Manage initrc injection
+    Initrc {
+        #[command(subcommand)]
+        command: Initrc,
+    },
 }
 
 #[derive(clap::Subcommand, Debug)]
@@ -554,7 +555,22 @@ mod kpm_cmd {
     }
 }
 
-#[allow(clippy::similar_names)]
+#[derive(clap::Subcommand, Debug)]
+enum Susfs {
+    /// Get SUSFS Status
+    Status,
+    /// Get SUSFS Version
+    Version,
+    /// Get SUSFS enable Features
+    Features,
+}
+
+#[derive(clap::Subcommand, Debug)]
+enum Initrc {
+    /// Regenerate preinit rc file
+    Refresh,
+}
+
 pub fn run() -> Result<()> {
     android_logger::init_once(
         Config::default()
@@ -894,6 +910,9 @@ pub fn run() -> Result<()> {
                 Kpm::Version => kpm::version(),
             }
         }
+        Commands::Initrc { command } => match command {
+            Initrc::Refresh => regenerate_preinit_rc(),
+        },
     };
 
     if let Err(e) = &result {
